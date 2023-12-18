fadfb0f059c55fe1d14f8b3ce3c72df14d70abf5
Ryan <ryan@galgon.name>
2023-12-17 16:50:26 -0800

    added debug

--- day1/day1.py
+++ day1/day1.py
@@ -1,10 +1,10 @@
-
+DEBUG = True 
 
 def extract_calibration_digits(line):
     digits = [int(d) for d in line if d.isdigit()]
     constructed_digit = int(str(digits[0]) + str(digits[-1]))
-    #debug
-    print(line + " found digits: " + str(digits) + " constructed digit: " + str(constructed_digit))
+    if DEBUG:
+        print(line + " found digits: " + str(digits) + " constructed digit: " + str(constructed_digit))
     return constructed_digit
 
 with open("input_day1.txt", "r") as file:
@@ -13,4 +13,4 @@ with open("input_day1.txt", "r") as file:
     for line in file:
         sum_constructed_digit += extract_calibration_digits(line)
         #print(line + " constructed digit: " + str(constructed_digit))
-print(sum_constructed_digit)
+print("total sum: " + str(sum_constructed_digit))
